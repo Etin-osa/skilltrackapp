bdce826915b30684ce081168370240f4509210d2
Etinosa <frederickade15@gmail.com>
2025-03-02 16:54:32 +0100

    First commit for skill track app

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet } from 'react-native';
+
+const Profile = () => {
+    return (
+        <ThemedView>
+            <ThemedText>Profile page!!!!!!</ThemedText>
+        </ThemedView>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Profile;
---
+++ app/(tabs)/skills.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Image, Platform } from 'react-native';
+
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+
+export default function TabTwoScreen() {
+    return (
+        <ThemedView>
+            <ThemedText>Skills !!!!!!!</ThemedText>
+        </ThemedView>
+    );
+}
+
+const styles = StyleSheet.create({});
